000e8a7feb77f92c31440265765cdf60fe9388df
s1042992 <40089665+s1042992@users.noreply.github.com>
2022-04-27 20:22:47 +0800

    Update GGAutofill.js

--- GGAutofill.js
+++ GGAutofill.js
@@ -1,28 +1,36 @@
-javascript:
+javascript: 
 function agreefunc() {
-    document.getElementById("62391608_516129376_label").click();
+    document.getElementById("66405067_542650090_label").click();
 }
+
 function employeeID() {
-    document.getElementById("62391605").setAttribute('value', 'XXXXXX');
+    document.getElementById("66405064").setAttribute('value', 'XXXXXX');
 }
+
 function temperature() {
-    document.getElementById("62391610_516129378_label").click();
-    document.getElementById("62391606").setAttribute('value', (Math.random() * (36.8 - 35.8) + 35.8).toFixed(1));
+    document.getElementById("66405069_542650092").click();
+    document.getElementById("66405065").setAttribute('value', (Math.random() * (36.8 - 35.8) + 35.8).toFixed(1));
 }
+
 function symptoms() {
-    document.getElementById("62391616_516129418").click();
+    document.getElementById("66405075_542650132").click();
+    document.getElementById("66405075_542650132").checked = true;
 }
+
 function rapidtest() {
-    document.getElementById("62391993_525542511_label").click();
+    document.getElementById("66405078_542650167").click();
 }
+
 function footprint() {
-    document.getElementById("62391615_516129447_label").click();
+    document.getElementById("66405074_542650161").click();
 }
+
 function injection() {
-    document.getElementById("62391617_516129442_label").click();
+    document.getElementById("66405076_542650157").click();
 }
+
 function plunge() {
-    document.getElementById("62391607_516129368_label").click();
+    document.getElementById("66405066_542650082").click();
 }
 symptoms();
 agreefunc();
